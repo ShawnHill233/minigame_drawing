56b7b2515402eaa4db56928f739aced566e67e6d
司晓翰 <a361162743@hotmail.com>
2018-12-21 00:53:12 +0800

    improve

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 import './js/weapp-adapter'
 
+var i, x, y, xx, yy;
 //保存上一个点
 var ppx, ppy;
 //保存当前点
@@ -33,19 +34,20 @@ wx.onTouchStart(function (e) {
 wx.onTouchStart(function (e) {
   console.log(e.touches)
   //画点
-  var cpx = e.touches[0].pageX;
-  var cpy = e.touches[0].pageY;
+  cpx = e.touches[0].pageX;
+  cpy = e.touches[0].pageY;
   drawpoint(cpx, cpy);
   //画线
-  var ppx = cpx;
-  var ppy = cpy;
+  ppx = cpx;
+  ppy = cpy;
   drawline(ppx, ppy, cpx, cpy);
 
   //画线
   wx.onTouchMove(function (e) {
+    draw_array = [];    //清空可绘点
     // console.log(e.touches)
-    var cpx = e.touches[0].pageX;
-    var cpy = e.touches[0].pageY;
+    cpx = e.touches[0].pageX;
+    cpy = e.touches[0].pageY;
     point_array.push([cpx, cpy]);
     drawline(ppx, ppy, cpx, cpy);
     ppx = cpx;
@@ -63,32 +65,28 @@ var drawpoint = function (x, y) {
 };
 
 //画线（点是不连续的)
-var drawline = function (x1, y1, x2, y2) {
+var drawline = function (ppx, ppy, cpx, cpy) {
   ctx.beginPath();
-  ctx.moveTo(x1, y1);
-  ctx.lineTo(x2, y2);
+  ctx.moveTo(ppx, ppy);
+  ctx.lineTo(cpx, cpy);
   ctx.stroke();
 
   //遍历已绘的点，将可取点存入array
-  for (var i = 0; i < point_array.length; i++) {
-    var x = point_array[i][0];
-    var y = point_array[i][1];
-    if (x < x2 + radius && x > x2 - radius) {
-      if (y < y2 + radius && y > y2 - radius) {
-        if ((x - x2) * (x - x2) + (y - y2) * (y - y2) < radius * radius) {
+  for (i = 0; i < point_array.length; i++) {
+    x = point_array[i][0];
+    y = point_array[i][1];
+    if (Math.abs(x - cpx) < radius && Math.abs(y - cpy) < radius) {
           draw_array.push([x, y]);
-        }
-      }
     }
   }
   // 取随机点
   draw_array = getRand(draw_array, 8);
   // 连接圆附近的随机点
   for (i = 0; i < draw_array.length; i++) {
-    var xx = draw_array[i][0];
-    var yy = draw_array[i][1];
+    xx = draw_array[i][0];
+    yy = draw_array[i][1];
     ctx.moveTo(xx, yy);
-    ctx.lineTo(x2, y2);
+    ctx.lineTo(cpx, cpy);
     ctx.lineWidth = 0.5;
     // ctx.setStrokeStyle('yellow')
     ctx.stroke();
